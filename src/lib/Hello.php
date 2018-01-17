494b419d5a37879b9a4612176a85535475c1ffe9
任广正 <guangzhengren@sina.com>
2018-01-17 16:17:12 +0800

    test

--- src/lib/Hello.php
+++ src/lib/Hello.php
@@ -4,11 +4,11 @@
  * @author: 任广正
  * @date: 18-1-17
  */
-namespace Monolog;
+namespace Test\lib;
 class Hello
 {
 	public function sayHello()
 	{
-		echo 'Hello World';
+		echo 'lib -> say Hello World';
 	}
 }
