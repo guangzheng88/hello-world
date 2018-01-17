7b664aec2c8bded01e20058cbfafae279f6468d8
任广正 <guangzhengren@sina.com>
2018-01-17 17:08:25 +0800

    test

---
+++ src/controller/Hello.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * composer test
+ * @author: 任广正
+ * @date: 18-1-17
+ */
+namespace controller;
+class Hello
+{
+	public function sayHello()
+	{
+		echo 'controller -> Hello World';
+	}
+}
