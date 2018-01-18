a17e22efb5e58d118ff15c57637fa175be8a3b1b
任广正 <guangzhengren@sina.com>
2018-01-18 14:07:10 +0800

    satis

---
+++ src/lib/test/Hello.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * composer test
+ * @author: 任广正
+ * @date: 18-1-17
+ */
+namespace lib\test;
+class Hello
+{
+	public function sayHello()
+	{
+		echo 'lib\test -> say Hello World';
+	}
+}
